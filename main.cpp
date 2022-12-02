c38eeefdcff1fcf49f45eabdcce25ff88d0aa234
Sebastián Armando Armando <82182557+FloresTM@users.noreply.github.com>
2022-12-02 04:37:02 +0000

    final

--- main.cpp
+++ main.cpp
@@ -1,83 +1,68 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Autores: David René Langarica Hernández - A01708936, Sebastian Flores Lemus - A01709229
+// Date: 01/12/2022
 //
 // =================================================================
 #include <iostream>
 #include <fstream>
 #include <cstring>
 #include <string>
-#include "exception.h"
-#include "hash.h"
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-//g++ -std=c++11 main.cpp -o app
-
-unsigned int myHash1(const string s) {
-	unsigned int acum = 0;
-	for (unsigned int i = 0; i < s.size(); i++) {
-		acum += (int) s[i];
-	}
-	return acum;
-}
-
-unsigned int myHash2(const string s) {
-	return (unsigned int) s[0];
-}
-
 int main(int argc, char* argv[]) {
 
-    // creamos archivo de input y output
+  // creamos archivo de input y output
   ifstream archivo1(argv[1]); 
   ofstream archivo2(argv[2]);
 
+  // usamos unordered_map para hacer el hash
+	unordered_map<string, int> hash;
+
   // palabras diccionario es el numero a buscar
   // descripcion_puestos es el segundo numero leido del archivo
-  int palabras_diccionario, descripciones_puestos, valor_dolares, suma_total;
-  //palabra es la key antes del valor
-  string palabra;
-
-  // lectura de datos
-  archivo1 >> palabras_diccionario >> descripciones_puestos;
-
-	HashTable<string, int> hash(palabras_diccionario, string("empty"), myHash1);
+  // valor_dolares es el numero que se suma al total
+  int palabras_diccionario, descripciones_puestos, valor_dolares;
   
-  for(int i = 0; i < palabras_diccionario; i++){
-    archivo1 >> palabra >> valor_dolares;
-    hash.put(string(palabra), valor_dolares);
-  }
+  // palabra es la key antes del valor
+  // descripcion_puesto son los parrafos que seran leidos
+  string palabra, descripcion_puesto;
 
-	//archivo2 << hash.toString() << "\n\n";
-  
-  for(int i = 0; i < descripciones_puestos; i++){
-    string descripcion_puesto;
-    suma_total = 0;
+  // lectura de datos del archivo input
+	archivo1 >> palabras_diccionario >> descripciones_puestos;
 
-    while(archivo1 >> descripcion_puesto)){
-      if(descripcion_puesto == "."){
-        break;
-      }
-      else {
-        suma_total += valor_dolares;
-      }
-      archivo2 << suma_total << endl;
+  // leemos e insertamos los datos al hash con 'insert'
+	for (int i = 0; i < palabras_diccionario; i++)
+	{
+		archivo1 >> descripcion_puesto >> valor_dolares;
+
+    // andimos key, value al map
+    hash.insert(make_pair(descripcion_puesto,valor_dolares));
+	}
+
+   while (descripciones_puestos--){
+		int suma_total = 0;
+
+		while (archivo1 >> descripcion_puesto){
+      // rompemos la lectura cuando encontramos un cambio de parrafo, en este caso separado por un punto
+  		if (descripcion_puesto == ".")
+  		break;
+
+      // hacemos la suma
+  		suma_total += hash[descripcion_puesto];
+  	}
+
+     // imprimimos resultado al archivo output
+  	archivo2 << suma_total << endl;
     }
-    
-  }
 
-  /*
-  for(int i = 0; i < descripciones_puestos; i++){
-    archivo2 << descripcion_puesto << "\n.";
-  }
-  */
-  
   archivo1.close();
   archivo2.close();
   
 	return 0;
 }
 
-//g++ -std=c++11 main.cpp -o app
